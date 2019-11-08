1f8226b12c700387520a5119fcd0cd203736c8d6
Lentil770 <42477416+Lentil770@users.noreply.github.com>
2019-11-07 21:36:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 $(function() {
     $('#js-shopping-list-form').on('submit', function(event) {
       event.preventDefault();
+      const newItem = $('#shopping-list-entry').val();
       $('#shopping-list-entry').val('');
-      const newItem = $('#shopping-list-entry').val()
-      $('.shopping-list').html(      `<li>
+      $('.shopping-list').append(      `<li>
         <span class="shopping-item">${newItem}</span>
         <div class="shopping-item-controls">
           <button class="shopping-item-toggle">
