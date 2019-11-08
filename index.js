5101ebf6308ac741f12f5f0c3eb334c7d574044d
Lentil770 <42477416+Lentil770@users.noreply.github.com>
2019-11-07 19:51:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(function() {
     $("button[type='submit']").on('submit', function(event) {
       event.preventDefault();
-      $('.shopping-list-entry').val('');
-      const newItem = $('.shopping-list-entry').val();
+      $('#shopping-list-entry').val('');
+      const newItem = $('#shopping-list-entry').val();
 
       $('.shopping-list').append( `     <li>
         <span class="shopping-item">${'newItem'}</span>
