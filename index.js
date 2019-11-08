9aaa47f457a3407f55bc7ba6fd076523f1444900
Lentil770 <42477416+Lentil770@users.noreply.github.com>
2019-11-07 20:19:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
 $(function() {
+    $('.shopping-list').html( `     <li>
+        <span class="shopping-item">apples</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+      <li>
+        <span class="shopping-item">oranges</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+      <li>
+        <span class="shopping-item shopping-item__checked">milk</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+      <li>
+        <span class="shopping-item">bread</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
     $('.js-submit').on('submit', function(event) {
       event.preventDefault();
       $('#shopping-list-entry').val('');
       const newItem = $('#shopping-list-entry').val();
       const oldList = $('.shopping-list').val();
-      $('.shopping-list').html(      `<li>
+      $('.shopping-list').append(      `<li>
         <span class="shopping-item">${newItem}</span>
         <div class="shopping-item-controls">
           <button class="shopping-item-toggle">
