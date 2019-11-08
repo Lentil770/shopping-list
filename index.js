8737a01c39c0b2726d7c5b271b0edf6b8136b3c3
Lentil770 <42477416+Lentil770@users.noreply.github.com>
2019-11-07 19:28:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ $(function() {
     });
 
   $('.shopping-item-toggle').click(function() {
-    $(this).closest('li').find('shopping-item').toggleClass(shopping-item__checked);
+    $(this).closest('li').find('shopping-item').toggleClass('shopping-item__checked');
   })
   $('.shopping-item-delete').click(function() {
     $(this).closest('li').remove();
